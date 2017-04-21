325d8288de62120b1261123bab71f240cf8a51ea
yaaroslaw <yaaroslaw@gmail.com>
2017-04-21 12:51:45 +0300

    Changed the default values of ValuesController.

--- HelloApiTemplateDemo/Controllers/ValuesController.cs
+++ HelloApiTemplateDemo/Controllers/ValuesController.cs
@@ -10,31 +10,46 @@ namespace HelloApiTemplateDemo.Controllers
     [Authorize]
     public class ValuesController : ApiController
     {
+        static List<string> data =initList();
+
+        private static List<string> initList()
+        {
+            var ret = new List<string>
+            {
+                "value1",
+                "value2"
+            };
+            return ret;
+        }
+
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return data;
         }
 
         // GET api/values/5
         public string Get(int id)
         {
-            return "value";
+            return data[id];
         }
 
         // POST api/values
         public void Post([FromBody]string value)
         {
+            data.Add(value);
         }
 
         // PUT api/values/5
         public void Put(int id, [FromBody]string value)
         {
+            data[id] = value;
         }
 
         // DELETE api/values/5
         public void Delete(int id)
         {
+            data.RemoveAt(id);
         }
     }
 }
